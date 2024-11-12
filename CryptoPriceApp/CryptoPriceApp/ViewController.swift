b4ee1524a2532f2fc954e87cc7939050a13707f3
eav1stfr <eavst123@gmail.com>
2024-11-12 20:56:50 +0500

    created project

---
+++ CryptoPriceApp/CryptoPriceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoPriceApp
+//
+//  Created by Александр Эм on 12.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
